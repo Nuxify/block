3418775011bc6909a8413967dd4769dafbf6b7c1
danvincent1122 <danvincent1122@gmail.com>
2022-06-14 14:00:41 +0800

    fix: lint

--- components/web3/index.ts
+++ components/web3/index.ts
@@ -1,4 +1,4 @@
-import ConnectWalletModal from './ConnectWalletModal.vue'
 import WalletHandler from '../web3/WalletHandler.vue'
+import ConnectWalletModal from './ConnectWalletModal.vue'
 
 export { ConnectWalletModal, WalletHandler }
